cf23b11fb8bcadfd43321a005831fd12844f2bc1
s10110128 <andyeung523@gmail.com>
2019-12-19 00:53:34 +0800

    home layput finish

--- lib/page/home.dart
+++ lib/page/home.dart
@@ -9,20 +9,87 @@ class Home extends StatefulWidget {
 class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-
     return Scaffold(
       backgroundColor: Colors.white,
       body: SafeArea(
-        child: Column(
-          crossAxisAlignment: CrossAxisAlignment.stretch,
-          mainAxisSize: MainAxisSize.max,
-          mainAxisAlignment: MainAxisAlignment.start,
-          children: <Widget>[
-            Image.asset(
-              'assets/logo.png', width: 100.0, height: 100.0,
+        child:
+          Center(
+            child: Container(
+              padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+              child: Column(
+                crossAxisAlignment: CrossAxisAlignment.center, 
+                mainAxisSize: MainAxisSize.max, 
+                mainAxisAlignment: MainAxisAlignment.start,
+                children: <Widget>[
+                  Image.asset(//Logo
+                    'assets/logo.png', width: 200.0, height: 100.0,
+                  ),
+                  Container(
+                    padding: EdgeInsets.fromLTRB(8, 30, 8, 30),
+                    child: Text(
+                      'Begin Your Investment Journey!',
+                      style: TextStyle(fontStyle: FontStyle.italic, color: Colors.grey[800], fontSize: 20),
+                      ),
+                  ),
+                  Image.asset( //Invesment Game
+                    'assets/inv_game.png', width: 100.0, height: 100.0,
+                  ),
+                  SizedBox(
+                      width: 250,
+                      // height: double.infinity,
+                      child: FlatButton(
+                        padding: EdgeInsets.all(12.0),
+                        color: Color.fromRGBO(159, 42, 51, 1),
+                        onPressed: () {
+                          /*...*/
+                        },
+                        child: Text(
+                          'Invesment Game',
+                          style: TextStyle(fontSize: 20.0,color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    Image.asset( //Chat Bot
+                      'assets/chatbot.png', width: 100.0, height: 100.0,
+                    ),
+                    SizedBox(
+                      width: 250,
+                      // height: double.infinity,
+                      child: FlatButton(
+                        padding: EdgeInsets.all(12.0),
+                        color: Color.fromRGBO(159, 42, 51, 1),
+                        onPressed: () {
+                          /*...*/
+                        },
+                        child: Text(
+                          'Chat Bot',
+                          style: TextStyle(fontSize: 20.0,color: Colors.white),
+                        ),
+                      ),
+                    ),
+                    
+                    Image.asset( //Dream Hub
+                    'assets/dream_hub.png', width: 100.0, height: 100.0,
+                    ),
+                    SizedBox(
+                      width: 250,
+                      // height: double.infinity,
+                      child: FlatButton(
+                        padding: EdgeInsets.all(12.0),
+                        color: Color.fromRGBO(159, 42, 51, 1),
+                        onPressed: () {
+                          /*...*/
+                        },
+                        child: Text(
+                          'Dream Hub',
+                          style: TextStyle(fontSize: 20.0,color: Colors.white),
+                        ),
+                      ),
+                    ),
+                ],
+              )
             ),
-          ],
-        )
+          )
       )
     );
     //return MaterialApp(
