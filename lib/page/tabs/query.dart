9de98f73ca1e4810b38488375414d04893f9cf89
s10110128 <andyeung523@gmail.com>
2019-12-20 06:29:54 +0800

    apiOK

--- lib/page/tabs/query.dart
+++ lib/page/tabs/query.dart
@@ -13,8 +13,9 @@ class Query extends StatefulWidget {
 
 class _QueryState extends State<Query> {
   StockAPI api = new StockAPI();
+  final myController = TextEditingController();
   int stock_code=0;
-  String name_en = '';
+  String name_en = 'n/a';
   double price = 0.0;
   double change = 0.0;
   double change_pct = 0.0;
@@ -25,7 +26,7 @@ class _QueryState extends State<Query> {
   void initState() {
     // TODO: implement initState
     stock_code=0;
-    name_en = '';
+    name_en = 'n/a';
     price = 0.0;
     change = 0.0;
     change_pct = 0.0;
@@ -40,11 +41,13 @@ class _QueryState extends State<Query> {
       child:Container(
         padding: EdgeInsets.fromLTRB(0, 0, 0,0),
         child: Scaffold(
+          resizeToAvoidBottomInset: false,
           appBar: AppBar(
             backgroundColor: Color.fromRGBO(159, 42, 51, 1),
             title: Text("Real-Time Stock Query"),
           ),
-          body: Column(
+          body: SingleChildScrollView(
+            child: Column(
             children: <Widget>[
               Row(
               children: <Widget>[
@@ -55,11 +58,10 @@ class _QueryState extends State<Query> {
                     width: 100,
                     //padding: EdgeInsets.fromLTRB(10, 30, 30, 15),
                     color: Color.fromRGBO(255, 210, 213, 1),
-                    child: Text('  Stock Code'),
+                    child: Center(child: Text('Stock Code')),
                     alignment: Alignment.centerLeft,
                   ),
                 ),
-                
                 Expanded(
                   flex: 6,
                   child: Container(
@@ -68,7 +70,7 @@ class _QueryState extends State<Query> {
                     padding: EdgeInsets.all(30.0),
                     color: Color.fromRGBO(240, 240, 240, 1),
                     child: TextFormField(
-
+                             controller: myController,
                              decoration: InputDecoration(
                                  hintText: 'Please search',
                                  fillColor: Color.fromRGBO(255, 210, 213, 1),
@@ -83,7 +85,17 @@ class _QueryState extends State<Query> {
                                  suffixIcon: IconButton(
                                      icon: Icon(Icons.search, color: Colors.black),
                                      onPressed:  ()  async {
-                                       await api.get_stock_info(stock_code); 
+                                       stock_code = int.parse(myController.text);
+                                       await api.get_stock_info(stock_code);
+                                       setState(() {
+                                         name_en = api.get_name_en();
+                                         price = api.get_price();
+                                         change = api.get_change();
+                                         change_pct = api.get_change_pct();
+                                         pe_ratio = api.get_pe_ratio();
+                                         turnover = api.get_turnover();
+                                         lot_size = 500;
+                                       });
                                      })),
                            ),
                     alignment: Alignment.centerLeft,
@@ -91,8 +103,199 @@ class _QueryState extends State<Query> {
                 ),
               ],
             ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    //padding: EdgeInsets.fromLTRB(10, 30, 30, 15),
+                    color: Color.fromRGBO(255, 210, 213, 1),
+                    child: Center(child: Text('Stock Code')),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+                
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    padding: EdgeInsets.all(30.0),
+                    color: Color.fromRGBO(240, 240, 240, 1),
+                    child: Text('$stock_code'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    //padding: EdgeInsets.fromLTRB(10, 30, 30, 15),
+                    color: Color.fromRGBO(255, 210, 213, 1),
+                    child: Center(child: Text('Name')),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+                
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    padding: EdgeInsets.all(30.0),
+                    color: Color.fromRGBO(240, 240, 240, 1),
+                    child: Text('$name_en'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    //padding: EdgeInsets.fromLTRB(10, 30, 30, 15),
+                    color: Color.fromRGBO(255, 210, 213, 1),
+                    child: Center(child: Text('Nominal')),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+                
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    padding: EdgeInsets.all(30.0),
+                    color: Color.fromRGBO(240, 240, 240, 1),
+                    child: Text('$price'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    //padding: EdgeInsets.fromLTRB(10, 30, 30, 15),
+                    color: Color.fromRGBO(255, 210, 213, 1),
+                    child: Center(child: Text('Change')),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+                
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    padding: EdgeInsets.all(30.0),
+                    color: Color.fromRGBO(240, 240, 240, 1),
+                    child: Text('$change'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    //padding: EdgeInsets.fromLTRB(10, 30, 30, 15),
+                    color: Color.fromRGBO(255, 210, 213, 1),
+                    child: Center(child: Text('Change %')),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+                
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    padding: EdgeInsets.all(30.0),
+                    color: Color.fromRGBO(240, 240, 240, 1),
+                    child: Text('$change_pct'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    //padding: EdgeInsets.fromLTRB(10, 30, 30, 15),
+                    color: Color.fromRGBO(255, 210, 213, 1),
+                    child: Center(child: Text('Turnover\n(Thousand)')),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+                
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    padding: EdgeInsets.all(30.0),
+                    color: Color.fromRGBO(240, 240, 240, 1),
+                    child: Text('$stock_code'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    //padding: EdgeInsets.fromLTRB(10, 30, 30, 15),
+                    color: Color.fromRGBO(255, 210, 213, 1),
+                    child: Center(child: Text('Lot Size')),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+                
+                Expanded(
+                  flex: 6,
+                  child: Container(
+                    height: 80,
+                    width: 100,
+                    padding: EdgeInsets.all(30.0),
+                    color: Color.fromRGBO(240, 240, 240, 1),
+                    child: Text('$lot_size'),
+                    alignment: Alignment.centerLeft,
+                  ),
+                ),
+              ],
+            ),//copy here
             ],
           ),
+          )
+          
           )
       ),
     );
