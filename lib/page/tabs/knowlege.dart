b67459cab34e5d14e27df1e47bce32e812615e2c
Teemo <hohinchan123@gmail.com>
2019-12-20 14:33:55 +0800

    added_photos

--- lib/page/tabs/knowlege.dart
+++ lib/page/tabs/knowlege.dart
@@ -1,15 +1,5 @@
 import 'package:flutter/material.dart';
-import 'package:invest_game/page/tabs/my_pro.dart';
-import '../home.dart';
-import 'customIcons.dart';
-import 'data.dart';
-import 'dart:math';
-import 'dart:io';
-import 'package:invest_game/model/login/custom_route.dart';
 // import 'package:oauth2/oauth2.dart' as oauth2;
-import 'package:oauth2/oauth2.dart';
-import 'package:invest_game/model/login/stockAPI.dart';
-import 'package:invest_game/page/tabs/query.dart';
 
 
 class Knowledge extends StatefulWidget {
@@ -18,63 +8,224 @@ class Knowledge extends StatefulWidget {
 }
 
 class _KnowlegeState extends State<Knowledge> {
+
+
   @override
   Widget build(BuildContext context) {
-    
-    return Container(
-      child: SafeArea(
-        child: Scaffold(
-          backgroundColor: Colors.white,
-          body: SingleChildScrollView(
-            child: Column(
+    return SingleChildScrollView(
+ 
+          child:Column(
+            
+            //padding: EdgeInsets.only(top: 12),
+            children :<Widget>[
+              
+              Row(
+              
               children: <Widget>[
-                Center(
-                  child: Container(
-                    margin: EdgeInsets.all(20),
-                    padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
-                    child: Column(  
-                      children: <Widget>[
-                        Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Container(
-                                height: 150,
-                                child: Text("Tips",style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 3.5)),
-                                //color: Colors.black,
-                              ),
-                            ),
-                            Expanded(
-                              child: Container(
-                                height: 200,
-                                child: CircleAvatar(
-                                          backgroundImage: AssetImage('assets/rocket.JPG'),
-                                        )
-                                //color: Colors.black,
-                              ),
-                            ),
-                          ],
-                        ),
-                        Row(
-                          children: <Widget>[
-                            Expanded(
-                              child: Container(
-                                height: 100,
-                                child: Text("You\'re on mpdule 1 of 10",style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1)),
-                                //color: Colors.black,
-                              ),
-                            )
-                          ],
-                        )
-                      ],
-                    ),
-                    //color: colours,
+                Expanded(
+                  flex: 2,
+                  child: ClipRRect(
+                    // borderRadius: BorderRadius.circular(50.0),
+                  child: new Container(
+                    child: Center(child: Text('Chapter 1', style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold),)),
+                    
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    height: 250.0,
+                    color: Colors.green,
                   ),
-                )
+                  ),
+                ),
+
+              ]
+            ),
+            Row(
+              
+              children: <Widget>[
+                Expanded(
+                  
+                  flex: 2,
+                  child: ClipRRect(
+                  child: new Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 0, 0),
+                    height: 10.0,
+                    color: Colors.red,
+                  ),
+                  ),
+                ),
+                
+                Expanded(
+                  
+                  flex: 4,
+                  child: ClipRRect(
+                  child: new Container(
+                    margin: EdgeInsets.fromLTRB(0,0, 10, 0),
+                    height: 10.0,
+                    color: Colors.grey[500],
+                    alignment: Alignment.centerLeft,
+                  ),
+                  ),
+                ),
               ],
             ),
+
+            Row(
+              
+              children: <Widget>[
+                Expanded(
+                  
+                  
+                  child: ClipRRect(
+                  child: new Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    height: 100.0,
+                    color: Colors.grey,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text("Investing Introduction     34%", style: TextStyle(fontSize: 30, color: Colors.white),),
+                    ),
+                  ),
+                  ),
+                ),
+                
+                
+              ],
+            ),
+
+
+            Row(
+              
+              children: <Widget>[
+                Expanded(
+                  flex: 2,
+                  child: ClipRRect(
+                    // borderRadius: BorderRadius.circular(50.0),
+                  child: new Container(
+                    child: Center(child: Text('Chapter 2', style: TextStyle(fontSize: 50.0, fontWeight: FontWeight.bold),)),
+                    
+                    margin: EdgeInsets.fromLTRB(10, 10, 10, 0),
+                    height: 250.0,
+                    color: Colors.red[200],
+                  ),
+                  ),
+                ),
+
+              ]
+            ),
+            Row(
+              
+              children: <Widget>[
+                
+                
+                Expanded(
+                  
+                  flex: 4,
+                  child: ClipRRect(
+                  child: new Container(
+                    margin: EdgeInsets.fromLTRB(10,0, 10, 0),
+                    height: 10.0,
+                    color: Colors.grey[500],
+                    alignment: Alignment.centerLeft,
+                  ),
+                  ),
+                ),
+              ],
+            ),
+            Row(
+              
+              children: <Widget>[
+                Expanded(
+                  
+                  flex: 2,
+                  child: ClipRRect(
+                  child: new Container(
+                    margin: EdgeInsets.fromLTRB(10, 0, 10, 0),
+                    height: 100.0,
+                    color: Colors.red,
+                    child: Padding(
+                      padding: const EdgeInsets.all(10.0),
+                      child: Text("Advanced Investing          0%", style: TextStyle(fontSize: 30, color: Colors.white),),
+                    ),
+
+                  ),
+                  ),
+                ),
+                
+                
+              ],
+            ),
+
+
+
+            ]
           ),
-        ),
-      ),
+
+      
     );
   }
 }
+  
+      
+            
+
+  
+
+
+    
+    
+//     return Container(
+//       child: SafeArea(
+//         child: Scaffold(
+//           backgroundColor: Colors.white,
+//           body: SingleChildScrollView(
+//             child: Column(
+//               children: <Widget>[
+//                 Center(
+//                   child: Container(
+//                     margin: EdgeInsets.all(20),
+//                     padding: EdgeInsets.fromLTRB(0, 20, 0, 0),
+//                     child: Column(  
+//                       children: <Widget>[
+//                         Row(
+//                           children: <Widget>[
+//                             Expanded(
+//                               child: Container(
+//                                 height: 150,
+//                                 child: Text("Tips",style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 3.5)),
+//                                 //color: Colors.black,
+//                               ),
+//                             ),
+//                             Expanded(
+//                               child: Container(
+//                                 height: 200,
+//                                 child: CircleAvatar(
+//                                           backgroundImage: AssetImage('assets/rocket.JPG'),
+//                                         )
+//                                 //color: Colors.black,
+//                               ),
+//                             ),
+//                           ],
+//                         ),
+//                         Row(
+//                           children: <Widget>[
+//                             Expanded(
+//                               child: Container(
+//                                 height: 100,
+//                                 child: Text("You\'re on mpdule 1 of 10",style: DefaultTextStyle.of(context).style.apply(fontSizeFactor: 1)),
+//                                 //color: Colors.black,
+//                               ),
+//                             )
+//                           ],
+//                         )
+//                       ],
+//                     ),
+//                     //color: colours,
+//                   ),
+//                 )
+//               ],
+//             ),
+//           ),
+//         ),
+//       ),
+//     );
+//   }
+// }
